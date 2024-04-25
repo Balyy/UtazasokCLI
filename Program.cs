1586bc2d034139402fd3966ee353551efb792a81
Virágh Balázs <viraghbalazs2005@gmail.com>
2024-04-25 10:12:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtazasokCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UtazasRepository.Path = "utazasok.csv";
+
+            foreach (var item in UtazasRepository.FindAll())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(UtazasRepository.FindById(5));
+
+            Utazas ujUtazas = new Utazas()
+            {
+                Id = 0,
+                Orszag = "Kuba",
+                Honap = 7,
+                Nap = 13,
+                Hossz = 7,
+                Ar = 300000,
+                Ellatas = "félpanzió"
+            };
+
+            UtazasRepository.Save(ujUtazas);
+
+            Console.WriteLine("utazasok.csv új elemmel: ");
+            foreach (var item in UtazasRepository.FindAll())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
